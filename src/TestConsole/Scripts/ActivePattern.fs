1538eca7db9931f2055e762a29dccaa3ce5d11e9
Mikus Miroslav <miroslav.mikus@outlook.com>
2020-06-03 10:18:54 +0200

    Add ActivePatterns

---
+++ src/TestConsole/Scripts/ActivePattern.fs
@@ -0,0 +1,22 @@
+﻿module ActivePattern
+
+    let (|Int|_|) (str:string) = 
+        match System.Int32.TryParse(str) with
+        | (true, int) -> Some(int)
+        | _ -> None
+
+    let (|Bool|_|) (str:string) = 
+        match System.Boolean.TryParse(str) with
+        | (true, int) -> Some(int)
+        | _ -> None
+
+
+    let testParse str = 
+        match str with 
+        | Int nr -> printfn "String is a number: %i" nr
+        | Bool b -> printfn "String is a bool: %b" b
+        | _ -> printfn "Can't recognize"
+
+    testParse "135"
+    testParse "true"
+    testParse "654sd"
