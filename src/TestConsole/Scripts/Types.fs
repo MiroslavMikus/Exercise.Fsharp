2b735c3f8767c7bd0efec1aceedbe8efbd6ab367
Mikus Miroslav <miroslav.mikus@outlook.com>
2020-06-09 17:28:04 +0200

    Add reflection example

--- src/TestConsole/Scripts/Types.fs
+++ src/TestConsole/Scripts/Types.fs
@@ -93,3 +93,16 @@ showTheNoiseAnAnimalMakes (dog.AsAnimal)
 
 let cat = Felix
 showTheNoiseAnAnimalMakes (cat.AsAnimal)
+
+////// Structs
+type Point2D =
+   struct
+      val X: float
+      val Y: float
+      new(x: float, y: float) = { X = x; Y = y }
+   end
+
+//test
+let p = Point2D()  // zero initialized
+let p2 = Point2D(2.0,3.0)  // explicitly initialized
+
