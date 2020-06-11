5340106c05fe23ad80303a167af96cc510570b27
Mikus Miroslav <miroslav.mikus@outlook.com>
2020-06-11 13:45:12 +0200

    Add state machine example

---
+++ src/TestConsole/Scripts/StateMachine.fs
@@ -0,0 +1,38 @@
+﻿namespace TimedOperation
+
+open System
+
+module StateMachine = 
+    type todo = unit
+    let todo () = ()
+
+    // state data
+    type ReadyData = Timed<TimeSpan list>
+
+    type ReceivedMessageData = Timed<todo>
+    
+    type MoMessageData = Timed<TimeSpan list>
+
+    // states
+    type PollingConsumer = 
+    | ReadyState of ReadyData
+    | ReceivedMessageState of ReceivedMessageData
+    | NoMessageState of MoMessageData
+    | StoppedState
+
+    let transformFromStopped  = StoppedState
+
+    let transitionFromNoMessage shouldIdle idle (nm : MoMessageData) = 
+        if shouldIdle nm
+        then idle () |> Untimed.withResult nm.Result |> ReadyState
+        else StoppedState
+
+    let transmisionFromReady shouldPoll poll (r: ReadyData) : PollingConsumer =
+        if shouldPoll r 
+        then 
+            let msg = poll ()
+            match msg.Result with
+            | Some _ -> msg |> Untimed.withResult () |> ReceivedMessageState
+            | None -> msg |> Untimed.withResult r.Result |>  NoMessageState
+        else StoppedState
+
